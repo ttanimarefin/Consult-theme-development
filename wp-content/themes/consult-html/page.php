cb4413ee227c2f33f94aacf08a1c9f0dbc331a54
ttanimarefin <ttanimarefin@gmail.com>
2021-04-07 01:30:46 +0600

    create page

--- wp-content/themes/consult-html/page.php
+++ wp-content/themes/consult-html/page.php
@@ -1,8 +1,8 @@
 <?php get_header();?>
 <?php
 while (have_posts()) : the_post();
-the_content();
 
+get_template_part( 'template-parts/page/content','page' );
        if(comments_open() || get_comments_number()):
                     comments_template();
        endif;
